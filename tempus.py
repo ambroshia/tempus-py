e78f93767acd9480016c4ee77f89cff867a101c2
ambroshia <56530123+ambroshia@users.noreply.github.com>
2021-07-12 17:51:28 +1000

    add function to handle multiple map query results

--- tempus.py
+++ tempus.py
@@ -37,10 +37,32 @@ def display_map(mapname): # takes the full map name as a string parameter
     except requests.exceptions.RequestException as e:  # all request errors inherit from RequestException
         raise SystemExit(e)
 
+def choose_one_map(maps): # prompts user to choose one map from 2-5 results
+    print(str(len(maps)) + ' partial matches found:')
+
+    for i in range(0, len(maps)): # range() is used because the index number needs to be printed
+        print(' ' + str(i+1) + '. ' + maps[i]["name"])
+
+    index = input('Enter the number next to the map you wish to view (!q to go back): ')
+
+    while index != '!q': # !q is the string that allows user to exit from the function
+
+        if index in ['1','2','3','4','5']: # first make sure user inputted string that is a number between 1-5
+            if int(index) <= len(maps): # since the prior statement ensures the input is an int between 1-5, the only other check is to make sure the index is not larger than the map query results
+                display_map(maps[int(index)-1]["name"]) # the map info can be safely fetched
+                break # after map info is displayed, return to search map function
+            else: # the inputted index does not exist in the map result dictionary
+                print('Invalid input, please enter one of the displayed numbers.')
+        else: # user input is not an integer between 1-5
+            print('Invalid input, please enter one of the displayed numbers.')
+        
+        index = input('Enter the number next to the map you wish to view (!q to go back): ') # reprint the input query for the loop
+    
+
 def search_map(): # handles looking up maps
     query = input('Search for a map (!q to go back): ').lower() # scanner to read input, convert to lower case
 
-    while query != '!q':
+    while query != '!q': # !q is the string that allows user to exit from the function
 
         try: # attempt to send request
             resp = requests.get('https://tempus.xyz/api/search/playersAndMaps/' + query) # only a maximum of 20 players and 5 maps will be returned by the API.
@@ -55,7 +77,7 @@ def search_map(): # handles looking up maps
                     display_map(j["maps"][0]["name"])
                     
                 else: # 2-5 results were found
-                    print('TEST')
+                    choose_one_map(j["maps"]) # pass dict of maps to function to handle multiple results
 
             else: # there is some sort of http error
                 print(resp.status_code + ' error') # output the error code
